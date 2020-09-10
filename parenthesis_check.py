c9060cc5e6fdb65719a5e33173b17f65a8230f6e
Sumit Jaswal <sumitjaswal01@gmail.com>
2020-09-10 10:30:46 +0530

    Add parenthesis_check.py

---
+++ parenthesis_check.py
@@ -0,0 +1,25 @@
+# Checking whether parenthesis are balanced or not.
+# Input  : {{{)))[]
+# Output : False
+
+class Solution:
+	def isValid(self,s):
+		open_paren = ['(', '{', '[']
+		close_paren = [')', '}', ']']
+		in_paren = []
+		out_paren = []
+
+		if len(s)%2 != 0:
+			return False
+		else:
+			for paren in s:
+				if paren in open_paren:
+					index_open = open_paren.index(paren)
+					in_paren.append(index_open)
+				if paren in close_paren:
+					index_close = close_paren.index(paren)
+					out_paren.append(index_close)
+			return sorted(in_paren)==sorted(out_paren)
+
+s="{{{))}(((}})[]"
+print(Solution().isValid(s))		
